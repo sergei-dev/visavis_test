1113f5d0a967e37f90f37759b0ce79eef2806e5e
Sergey Antoniuk <sergey.antonuk@horoshop.com.ua>
2020-06-26 13:02:33 +0300

    fix build pathes

--- build/js/main.js
+++ build/js/main.js
@@ -10440,31 +10440,31 @@ function progressInit() {
     data: {
       progressItems: [{
         id: 0,
-        avatar: '/img/circle.svg',
+        avatar: 'img/circle.svg',
         title: '0 Lorem Ipsum is simply dummy text',
         text: "It is a long established fact that a reader will be distracted by the\n                    readable content of a page when looking at its layout.",
         sublist: ['Lorem Ipsum has been the industry', 'Standard dummy text ever since', 'But also the leap into electronic typesetting']
       }, {
         id: 1,
-        avatar: '/img/circle.svg',
+        avatar: 'img/circle.svg',
         title: '1 Lorem Ipsum is simply dummy text',
         text: "It is a long established fact that a reader will be distracted by the\n                    readable content of a page when looking at its layout.",
         sublist: ['Lorem Ipsum has been the industry', 'Standard dummy text ever since', 'But also the leap into electronic typesetting', 'It was popularised in the 1960s']
       }, {
         id: 2,
-        avatar: '/img/circle.svg',
+        avatar: 'img/circle.svg',
         title: '2 Lorem Ipsum is simply dummy text',
         text: "It is a long established fact that a reader will be distracted by the\n                    readable content of a page when looking at its layout.",
         sublist: ['Lorem Ipsum has been the industry', 'Standard dummy text ever since', 'But also the leap into electronic typesetting']
       }, {
         id: 3,
-        avatar: '/img/circle.svg',
+        avatar: 'img/circle.svg',
         title: '3 Lorem Ipsum is simply dummy text',
         text: "It is a long established fact that a reader will be distracted by the\n                    readable content of a page when looking at its layout.",
         sublist: ['Lorem Ipsum has been the industry', 'Standard dummy text ever since', 'But also the leap into electronic typesetting']
       }, {
         id: 4,
-        avatar: '/img/circle.svg',
+        avatar: 'img/circle.svg',
         title: '4 Lorem Ipsum is simply dummy text',
         text: "It is a long established fact that a reader will be distracted by the\n                    readable content of a page when looking at its layout.",
         sublist: ['Lorem Ipsum has been the industry', 'Standard dummy text ever since', 'But also the leap into electronic typesetting']
@@ -10476,7 +10476,7 @@ function progressInit() {
       this.$nextTick(function () {
         _this.randomItems();
 
-        var loaderHtml = "\n                    <div class=\"progress__anim-rotate\">\n                        <img src=\"/img/circle-rotate.svg\" alt=\"\" />\n                    </div>";
+        var loaderHtml = "\n                    <div class=\"progress__anim-rotate\">\n                        <img src=\"img/circle-rotate.svg\" alt=\"\" />\n                    </div>";
         document.querySelectorAll('.progress__item')[0].querySelector('.progress__img-wrap').insertAdjacentHTML('beforeend', loaderHtml);
       });
     },
